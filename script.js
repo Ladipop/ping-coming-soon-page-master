9f9c92a87466fe61f4d7eacb5d61fe0f42f10b78
Ladipop <109416795+Ladipop@users.noreply.github.com>
2024-06-23 14:30:31 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ const emailInput = document.querySelector(".email-field");
 const EmailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const btn = document.querySelector(".btn");
 const errorContainer = document.querySelector(".error-container");
+const overlay = document.querySelector(".overlay");
+const popUp = document.querySelector(".pop-up");
+const emailName = document.querySelector(".email-name");
 
 const validate = (e) => {
   e.preventDefault();
@@ -12,20 +15,40 @@ const validate = (e) => {
   if (!EmailRegex.test(email)) {
     emailInput.style.borderColor = "Red";
     errorContainer.textContent = "Please input a valid email address";
+    return;
   } else {
+    popUp.classList.remove("animate__backOutDown", "animate__animated");
+    popUp.classList.add("visible-popup");
+    overlay.classList.add("overlayed");
     emailInput.style.borderColor = "initial";
     errorContainer.textContent = "";
+    emailName.textContent = email;
+    popUp.classList.add("animate__backInDown", "animate__animated");
+
+    setTimeout(() => {
+      popUp.style.top = "35%";
+      popUp.style.transform = "translate(-50%, -50%)";
+    });
   }
 };
-
 btn.addEventListener("click", validate);
 
-// const container = document.querySelector(".container");
+const cancelButton = document.querySelector(".fa-xmark");
+const cancel = () => {
+  popUp.classList.remove("animate__backInDown", "animate__animated");
+  overlay.classList.remove("overlayed");
+  popUp.classList.add("animate__backOutDown", "animate__animated");
+  setTimeout(() => {
+    popUp.classList.remove("visible-popup");
+  }, 500);
+};
+cancelButton.addEventListener("click", cancel);
 
-// const hover = () => {
-//   container.style.backgroundColor = getComputedStyle(
-//     document.documentElement
-//   ).getPropertyValue("--Blue");
-// };
-
-// container.addEventListener("mouseover", hover);
+const cancelOverlay = () => {
+  overlay.classList.remove("overlayed");
+  popUp.style.display = "none";
+  setTimeout(() => {
+    popUp.classList.remove("visible-popup");
+  }, 500);
+};
+overlay.addEventListener("click", cancel);
